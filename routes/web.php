d13674063b29beab30d3930a2491680a64816f38
Houndonougbo Bilal Ambroise <contact@ambroisehdn.me>
2023-08-14 22:40:26 +0200

    add ex

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ex', function () {
+    return "Hello ! ";
+});
+
 Route::get('api/documentation', function () {
     return view('vendor/l5-swagger/index');
 });
