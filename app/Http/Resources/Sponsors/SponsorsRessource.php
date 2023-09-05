ce76052c4770ef1fb60a8470f598f42877e3f4ff
TWIMM <twimm@gmail.com>
2023-09-05 14:38:36 +0100

    Tech fixes

--- app/Http/Resources/Sponsors/SponsorsRessource.php
+++ app/Http/Resources/Sponsors/SponsorsRessource.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Resources\Sponsors;
 
+use App\Http\Controllers\Sponsors\SponsorController;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 
 class SponsorsRessource extends JsonResource
 {
-    private function setAsset(string $assets){
+    private function setAsset(string $userId, string $eventId, array $assets){
         $newAssets = [];
-        array_push($newAssets,Storage::disk('public')->url($assets));
+        foreach ($assets as $asset) {
+            $path = $userId.'/'.SponsorController::STORAGE_EVENT.'/'.$eventId.'/'.SponsorController::STORAGE_SPONSOR;
+            if(Storage::disk('public')->exists($path)){
+                $path = $path.'/'.SponsorController::STORAGE_SPONSOR_LOGO.'/';
+                $newAssets['original'] = asset(Storage::url($path.$asset));
+                foreach (SponsorController::STORAGE_FORMATS as $format){
+                    $newAssets['resizes'][] = asset(Storage::url($path.'resize/'.$format.'-'.$asset));
+                }
+
+            }
+
+        }
 
         return $newAssets;
     }
@@ -26,7 +38,7 @@ class SponsorsRessource extends JsonResource
             "_id" => $this->_id,
             "name" =>  $this->name,
             "type" =>  $this->type,
-            "logo" => $this -> setAsset($this->logo),
+            "logo" => $this->setAsset($this -> event ->user->_id ,$this -> event->_id,$this->logo),
             "activity_sector" =>  $this->activity_sector,
             "description" =>  $this->description,
             "event" =>  $this ->  event,
